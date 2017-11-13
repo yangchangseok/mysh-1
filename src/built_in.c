cd017962463aa6c2dfe067a6763770dfb58b82d5
yangchangseok <32148670+yangchangseok@users.noreply.github.com>
2017-11-13 12:48:40 +0900

    built_in.c

--- src/built_in.c
+++ src/built_in.c
@@ -7,6 +7,7 @@
 #include <linux/limits.h>
 
 #include "built_in.h"
+#include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
@@ -32,13 +33,22 @@ int do_pwd(int argc, char** argv) {
   return 0;
 }
 
+#include <unistd.h>
+#include <sys/wait.h>
 int do_fg(int argc, char** argv) {
-  if (!validate_fg_argv(argc, argv))
-    return -1;
+    if (!validate_fg_argv(argc, argv))
+        return -1;
 
-  // TODO: Fill this.
+    if (s_bgChildPid != -1) {   // if has background child process
+        printf("%d\n", s_bgChildPid);
 
-  return 0;
+        s_isForeground = 1;             // set foreground flag for handle SIGINT
+        signal(SIGCHLD, SIG_IGN);       // ignore SIGCHLD sinal
+        waitpid(s_bgChildPid, NULL, 0); // block until s_bgChildPid terminated
+        s_bgChildPid = -1;              // clear background process id
+    }
+
+    return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
