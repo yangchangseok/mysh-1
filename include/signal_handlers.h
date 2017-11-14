e0f90a4a49a1717f737e964b5495aeaf9954ea0d
yangchangseok <32148670+yangchangseok@users.noreply.github.com>
2017-11-14 18:27:58 +0900

    signal_handlers.h

--- include/signal_handlers.h
+++ include/signal_handlers.h
@@ -1,8 +1,12 @@
 #ifndef SIGNAL_H_
 #define SIGNAL_H_
 
+#include <signal.h>
+
 void catch_sigint(int signalNo);
 
 void catch_sigtstp(int signalNo);
+
+void catch_sigchld(int signalNo);
 
 #endif // SIGNAL_H_
