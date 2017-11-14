0133c182258bc5a9a2375be061a4d7409852b5ba
yangchangseok <32148670+yangchangseok@users.noreply.github.com>
2017-11-14 18:27:19 +0900

    commands.h

--- include/commands.h
+++ include/commands.h
@@ -11,4 +11,18 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512]);
 
 void free_commands(int n_commands, struct single_command (*commands)[512]);
 
+#include <stdbool.h>
+static const char* const PATH[] = {
+    "/usr/local/bin",
+    "/usr/bin",
+    "/bin",
+    "/usr/sbin",
+    "/sbin",
+    0,  // point NULL for indicating end of path list
+};
+extern int s_bgChildPid;
+extern int s_isForeground;
+   
+int customEvaluation(struct single_command* command);
+
 #endif // MYSH_COMMANDS_H_
